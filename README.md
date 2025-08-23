85b07904dd31d09615d7e8a9d4cd5a682f629c6d
cygo-3x <edwardtsheposibeko302@gmail.com>
2025-08-23 21:34:28 +0200

    Day 4 learning C in public

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# c-play
+# C-Play
 Learning C in public.
+
+## Repository Structure
+- **book-practice** Exercises and examples from programming books.
+- **online-practice** Exercises from online tutorials, coding platforms, and videos.
+- **experiments** Personal exploratory code and quick tests.
+- **projects** Larger, self-contained programs and applications.
+
+*** Be patient with me. I am still learning all of this here. ***
+
+In the meantime, please follow me on X ([@cygo_3x](https://x.com/cygo_3x)).
---
+++ online-practice/youtube/circle-calculator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    // CIRCLE CALCULATOR
+
+    // declare variables
+    double radius = 0.0;
+    double area = 0.0;
+    double surfaceArea = 0.0;
+    double volume = 0.0;
+    const double Pi = 3.14159;
+
+    /* calculate: area; surface area; volume
+        and give output (answer)
+    */
+    printf("Enter the radius: ");
+    scanf("%lf", &radius);
+    
+    area = Pi * pow(radius, 2);
+    printf("Area: %.2lf\n", area);
+
+    surfaceArea = 4.0 * area;
+    printf("Surface Area: %.2lf\n", surfaceArea);
+
+    volume = (4.0 / 3.0) * Pi * pow(radius, 3);
+    printf("Volume: %.2lf\n", volume);
+
+    return 0;
+}
